78bf864460e95e1b634a54d124960c83c4fc4a61
Berend Romijn <berend.romijn@hva.nl>
2026-05-05 11:28:04 +0200

    iets moeilijkere variant gemaakt. vraag 2 getallen en vraag 1 van 2 operators

--- app.py
+++ app.py
@@ -1,6 +1,22 @@
 ## simpelste variant van een rekenmachine. vraag 2 getallen waarna deze allebij sowieso worden opgeteld.
+# a = input("geef getal nummer 1 op: ")
+# b = input("geef getal nummer 2 op: ")
+
+# c = int(a) + int(b)
+# print("Het resultaat is:", c)
+
+## iets moeilijkere variant van een rekenmachine. vraag 2 getallen en een operator waarna deze de bewerking uitvoert.
 a = input("geef getal nummer 1 op: ")
 b = input("geef getal nummer 2 op: ")
+vo = input("geef op of je plus of min wilt: ")
 
-c = int(a) + int(b)
-print("Het resultaat is:", c)
+match vo:
+    case "plus":
+        c = int(a) + int(b)
+        print("Het resultaat is:", c)
+    case "min":
+        c = int(a) - int(b)
+        print("Het resultaat is:", c)
+    case _:
+        print("Ongeldige operator, gebruik 'plus' of 'min'.")
+
