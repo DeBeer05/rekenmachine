409982e56fe5d73d0118547148a0400c9fe436d9
Berend Romijn <berend.romijn@hva.nl>
2026-05-05 11:24:19 +0200

    stap 1 van de rekenmachine gemaakt

---
+++ app.py
@@ -0,0 +1,6 @@
+## simpelste variant van een rekenmachine. vraag 2 getallen waarna deze allebij sowieso worden opgeteld.
+a = input("geef getal nummer 1 op: ")
+b = input("geef getal nummer 2 op: ")
+
+c = int(a) + int(b)
+print("Het resultaat is:", c)
